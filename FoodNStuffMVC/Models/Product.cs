33dc3497d934432071f77b8febef3d65292cb94f
Benjamin Durham <ben.dev033@gmail.com>
2019-06-25 11:23:20 -0400

    first commit

---
+++ FoodNStuffMVC/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FoodNStuffMVC.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int InventoryCount { get; set; }
+
+
+    }
+}
